3acd449bbf13ea40cb4c268cc5599d5447570cce
moldir <moldirshb@gmail.com>
2023-01-05 02:18:15 -0600

    added modal styling

--- client/js/tourList.js
+++ client/js/tourList.js
@@ -1,17 +1,15 @@
 let postTours = document.querySelector(".tours");
-let addBtn = document.querySelector('.btn.post')
-console.log(addBtn)
+let addBtn = document.querySelector(".btn.post");
 const openModalButtons = document.querySelectorAll("[data-modal-target]");
 const closeModalButtons = document.querySelectorAll("[data-close-button]");
 const overlay = document.getElementById("overlay");
 
-
 const baseURL = `http://localhost:4000`;
 
 const createTourCard = (tours) => {
-       const newTourCard = document.createElement("div");
-       newTourCard.classList.add("box");
-    newTourCard.innerHTML = `
+  const newTourCard = document.createElement("div");
+  newTourCard.classList.add("box");
+  newTourCard.innerHTML = `
 <div>
 
 <h3 class="title">${tours.name}</h3>
@@ -22,40 +20,31 @@ const createTourCard = (tours) => {
             <div>
             <h4>$${tours.price}</h4>
             <div>
-       
             </div>
-
             </div>
-              <a href="#" class="btn">Not Interested</a>
-
+              <button onclick="deleteTour(${tours.id})" class="btn">Not Interested</button>
               </div>
     `;
-postTours.appendChild(newTourCard);
-}
+  postTours.appendChild(newTourCard);
+};
 
 const displayTours = (arr) => {
-    arr.map((el) => {
-      console.log(el);
-      createTourCard(el)
-    });
-}
+  arr.map((el) => {
+    console.log(el);
+    createTourCard(el);
+  });
+};
 
 const getAllTours = () => {
-    axios
-      .get(`${baseURL}/getTours`)
-      .then((res) => {
-        displayTours(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-}
-
-
-
-
-
-
+  axios
+    .get(`${baseURL}/getTours`)
+    .then((res) => {
+      displayTours(res.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 openModalButtons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -90,38 +79,51 @@ function closeModal(modal) {
   overlay.classList.remove("active");
 }
 
-
-
 const addTour = () => {
-  postTours.innerHTML = ""
+  postTours.innerHTML = "";
 
-  const postModal = document.querySelector('.modal-body')
-  const name = document.querySelector('#titleInput')
-  const info = document.querySelector('#infoInput')
-  const image = document.querySelector('#imageInput')
-  const price = document.querySelector('#priceInput')
- 
+  const postModal = document.querySelector(".modal-body");
+  const name = document.querySelector("#titleInput");
+  const info = document.querySelector("#infoInput");
+  const image = document.querySelector("#imageInput");
+  const price = document.querySelector("#priceInput");
+
   let bodyObj = {
     name: name.value,
     info: info.value,
     image: image.value,
-    price: price.value
+    price: price.value,
   };
 
-
-  console.log(bodyObj)
+  console.log(bodyObj);
 
   axios
     .post(`${baseURL}/addTours`, bodyObj)
     .then((res) => {
+      name.value = "";
+      info.value = "";
+      image.value = "";
+      price.value = "";
+
       displayTours(res.data);
     })
     .catch((err) => {
       console.log(err);
     });
-}
+};
 
+const deleteTour = (id) => {
+  axios
+    .delete(`${baseURL}/deleteTour/${id}`)
+    .then((res) => {
+      postTours.innerHTML = "";
+      displayTours(res.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-addBtn.addEventListener('click', addTour )
+addBtn.addEventListener("click", addTour);
 
 getAllTours();
