50b39912cf24d748ac3fe53ec90324a40b23d0c5
moldir <moldirshb@gmail.com>
2023-01-05 20:49:53 -0600

    added wheather

---
+++ client/js/wheather.js
@@ -0,0 +1,57 @@
+let weather = {
+    apiKey: "09702372096301bdc5ae582b4c349426",
+
+  fetchWeather: function (city) {
+    axios
+      .get(
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+          city +
+          "&units=metric&appid=" +
+          this.apiKey
+      )
+      .then((res) => {
+        this.displayWeather(res.data);
+        // if (!response.ok) {
+        //   alert("No weather found.");
+        //   throw new Error("No weather found.");
+      })
+      .catch((err) => {
+              console.log(err);
+      });
+ 
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "Wind speed: " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+ 
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+weather.fetchWeather("Denver");
